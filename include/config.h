90ff638a3d5b16aa4061a7677983aca9e45ea42c
Alexander Vatti <alexander.vatti@jadaktech.com>
2021-04-05 17:58:14 +0530

    Initial commit for mtf-mapper

---
+++ include/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define mtfmapper_VERSION_MAJOR 0
+#define mtfmapper_VERSION_MINOR 7
+#define mtfmapper_VERSION_SUB 0
+
+#define genrectangle_VERSION_MAJOR 1
+#define genrectangle_VERSION_MINOR 2
+
+#define mtfmapper_ZLIB_FOUND 0
+
+#include <string>
+const std::string mtfmapper_HTML_HELP_DIR = "@HTML_HELP_DIR@/html/mtf_mapper_user_guide.html";
+
+#endif
